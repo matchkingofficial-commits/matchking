f47de3d4521e79d50c3deda4418eec32526cd8d5
fxpro254-hue <fxpro254@gmail.com>
2026-08-10 00:32:47 +0300

    price and telegram whatsapp

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // MatchKing Configuration
-const WHATSAPP_NUMBER = "+27603002724";
+const WHATSAPP_NUMBER = "t.me/matcchkingofficial";
 const USDT_ADDRESS = "TNbDnfW5D6TTBoW22AorR7tyS1n4qojfKY";
 
 // Track current purchase amount
